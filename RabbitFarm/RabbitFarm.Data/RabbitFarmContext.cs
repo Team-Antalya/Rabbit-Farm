7a7f24c5c95167a63c5cc65c8379b05f742d236b
Nina Markova <ninamarkoff@users.noreply.github.com>
2015-04-13 06:17:27 +0300

    Test models;

--- RabbitFarm/RabbitFarm.Data/RabbitFarmContext.cs
+++ RabbitFarm/RabbitFarm.Data/RabbitFarmContext.cs
@@ -9,16 +9,16 @@
 
     public class RabbitFarmContext : IdentityDbContext<User>
     {
-        //public const string ConnectionStringSQL = "Server=.;Database=RabbitFarm;Integrated Security=True;";
-        //public const string ConnectionStringAzure = "Server=ibz4rymk74.database.windows.net;Database=Antalya;Persist Security Info=True;User ID=antalya;Password=Parola123;";
-        //public const string ConnectionStringLocalDB = "Server=(localdb)\v11.0;Database=RabbitFarm;Integrated Security=True;";
+        public const string ConnectionStringSQL = "Server=.;Database=RabbitFarm;Integrated Security=True;";
+        public const string ConnectionStringAzure = "Server=ibz4rymk74.database.windows.net;Database=Antalya;Persist Security Info=True;User ID=antalya;Password=Parola123;";
+        public const string ConnectionStringLocalDB = "Server=(localdb)\v11.0;Database=RabbitFarm;Integrated Security=True;";
 
         // DefaultConnection
         // RabbitFarmConn
         public RabbitFarmContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base(ConnectionStringSQL, throwIfV1Schema: false)
         {
-            Database.SetInitializer(new DropCreateDatabaseAlways<RabbitFarmContext>());
+            //Database.SetInitializer(new DropCreateDatabaseAlways<RabbitFarmContext>());
         }
 
         public static RabbitFarmContext Create()
---
+++ RabbitFarm/RabbitFarm.WebAPI/DataModels/AcquisitionModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.Ajax.Utilities;
+using RabbitFarm.Models;
+
+namespace RabbitFarm.WebAPI.DataModels
+{
+    public class AcquisitionModel
+    {
+        public int Id { get; set; }
+
+        public DateTime AcquisitionDate { get; set; }
+
+        public static Expression<Func<Acquisition, AcquisitionModel>> AcquisitionToViewModel
+        {
+            get
+            {
+                return a => new AcquisitionModel
+                {
+                    Id = a.Id,
+                    AcquisitionDate = a.AcquisitionDate
+                };
+            }
+        }
+    }
+}
