22fd293f95f396afb51d8183e7323e0989d1281a
Karim <unbelt@outlook.com>
2015-04-14 20:41:39 +0300

    Validation added for the View Models

--- RabbitFarm/RabbitFarm.WebAPI/DataModels/CageModel.cs
+++ RabbitFarm/RabbitFarm.WebAPI/DataModels/CageModel.cs
@@ -1,13 +1,24 @@
 ﻿namespace RabbitFarm.WebAPI.DataModels
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class CageModel
     {
+        public int Id { get; set; }
+
+        [Range(0, double.MaxValue)]
+        [Required(ErrorMessage = "Cage Width is required")]
         public double Width { get; set; }
 
+        [Range(0, double.MaxValue)]
+        [Required(ErrorMessage = "Cage Height is required")]
         public double Height { get; set; }
 
+        [Range(0, double.MaxValue)]
+        [Required(ErrorMessage = "Cage Length is required")]
         public double Length { get; set; }
 
-        public FarmModel Farm { get; set; }
+        [Required(ErrorMessage = "FarmId is required")]
+        public int FarmId { get; set; }
     }
 }
--- RabbitFarm/RabbitFarm.WebAPI/DataModels/RealizationModel.cs
+++ RabbitFarm/RabbitFarm.WebAPI/DataModels/RealizationModel.cs
@@ -1,21 +1,32 @@
 ﻿namespace RabbitFarm.WebAPI.DataModels
 {
     using System;
+    using System.ComponentModel.DataAnnotations;
 
     using RabbitFarm.Models;
 
     public class RealizationModel
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Realization Date is required")]
         public DateTime RealizationDate { get; set; }
 
+        [Required(ErrorMessage = "Realization Channel is required")]
         public RealizationChannel RealizationChannel { get; set; }
+
+        [Required(ErrorMessage = "RabbitId is required")]
+        public int RabbitId { get; set; }
 
         public RabbitModel Rabbit { get; set; }
 
+        [Range(0, double.MaxValue, ErrorMessage = "Realization Live Weight must be positive number")]
         public double? LiveWeight { get; set; }
 
+        [Range(0, double.MaxValue, ErrorMessage = "Realization Price must be positive number")]
         public decimal? Price { get; set; }
 
-        public FarmModel Farm { get; set; }
+        [Required(ErrorMessage = "FarmId is required")]
+        public int FarmId { get; set; }
     }
 }
