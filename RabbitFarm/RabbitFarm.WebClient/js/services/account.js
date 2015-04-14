1241a35e01de6d64af7dbca06f8ee5b794fff90e
ttitto <ttitto_prog@vfemail.net>
2015-04-14 21:48:03 +0300

    merge

--- RabbitFarm/RabbitFarm.WebClient/js/services/account.js
+++ RabbitFarm/RabbitFarm.WebClient/js/services/account.js
@@ -2,20 +2,37 @@
 
 define(['angular', 'angularCookies', 'services/resource'], function (angular) {
     angular.module('App.Account', ['ngCookies'])
-        .factory('account', ['$rootScope', '$cookieStore', 'resource',
-            function ($rootScope, $cookieStore, resource) {
+        .factory('account', ['$rootScope', '$cookieStore', 'resource', '$http', '$q', 'service',
+            function ($rootScope, $cookieStore, resource, $http, $q, service) {
 
-                var login = function (user) {
+                var userServiceUrl = service.url,
+                    login = function (user) {
                         console.log(user);
+                        $http.post(userServiceUrl + '/Token', user)
+                            .success(function (userLoginData) {
+                                d.resolve(userLoginData);
+                            })
+                            .error(function (loginErr) {
+                                d.reject(loginErr);
+                            });
                     },
                     logout = function () {
                         console.log('Logged out');
                     },
                     register = function (user) {
+                        var d = $q.defer();
+                        $http.post(userServiceUrl + '/account/register', user)
+                            .success(function (userRegistrationData) {
+                                d.resolve(userRegistrationData);
+                            })
+                            .error(function (registrationErr) {
+                                d.reject(registrationErr);
+                            });
                         console.log(user);
                     },
                     redirect = function (path) {
                         console.log('Redirect');
+
                     },
                     isAuthenticated = function () {
                         console.log(false);
@@ -30,5 +47,5 @@ define(['angular', 'angularCookies', 'services/resource'], function (angular) {
                 };
             }
         ]
-    )
+        )
 });
